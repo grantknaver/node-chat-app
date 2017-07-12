3b6c4f9e452c8e7478f406c9720f3b27b3025e90
grantknaver <grantknaver@gmail.com>
2017-07-11 22:00:15 -0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Grant',
-    text: 'Yup, that works for me.'
-  });//emit createMessage
-
 });// on connect
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'John',
-    text: 'See you then',
-    createdAt: 123123
-  });//emit newMessage
-
   socket.on('createMessage', (message) => {
-    console.log('Created Message', message);
+    console.log('createMessage', message);
+
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });//io.emit createMessage
+
   });//on createMessage
 
   socket.on('disconnect', () => {
